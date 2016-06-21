34d59aa9c97f446acc7763864f4292a0e9d16e33
yaoyang <yaoyang@mobvoi.com>
2016-06-21 21:46:35 +0800

    database

--- lingluren.sql
+++ lingluren.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: 2016-06-19 16:34:21
+-- Generation Time: 2016-06-21 21:40:17
 -- 服务器版本： 5.5.47-MariaDB
 -- PHP Version: 5.4.16
 
@@ -38,7 +38,14 @@ CREATE TABLE IF NOT EXISTS `tbl_article` (
   `url` varchar(300) NOT NULL,
   `indexs` tinyint(4) NOT NULL,
   `position` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `tbl_article`
+--
+
+INSERT INTO `tbl_article` (`id`, `title`, `des`, `img`, `path`, `url`, `indexs`, `position`) VALUES
+(1, 'sdf', 'sdfdsf', '14665127615880.png', 'uploads/img/article/14665127615880.png', 'http://fanyi.baidu.com', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -58,7 +65,42 @@ CREATE TABLE IF NOT EXISTS `tbl_migration` (
 
 INSERT INTO `tbl_migration` (`version`, `apply_time`) VALUES
 ('m000000_000000_base', 1466324385),
-('m130524_201442_init', 1466324391);
+('m130524_201442_init', 1466324391),
+('m140209_132017_init', 1466515223),
+('m140403_174025_create_account_table', 1466515224),
+('m140504_113157_update_tables', 1466515224),
+('m140504_130429_create_token_table', 1466515225),
+('m140830_171933_fix_ip_field', 1466515226),
+('m140830_172703_change_account_table_name', 1466515226),
+('m141222_110026_update_ip_field', 1466515226),
+('m141222_135246_alter_username_length', 1466515226),
+('m150614_103145_update_social_account_table', 1466515226),
+('m150623_212711_fix_username_notnull', 1466515227);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_profile`
+--
+
+DROP TABLE IF EXISTS `tbl_profile`;
+CREATE TABLE IF NOT EXISTS `tbl_profile` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `public_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_id` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bio` text COLLATE utf8_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- 转存表中的数据 `tbl_profile`
+--
+
+INSERT INTO `tbl_profile` (`user_id`, `name`, `public_email`, `gravatar_email`, `gravatar_id`, `location`, `website`, `bio`) VALUES
+(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -74,7 +116,47 @@ CREATE TABLE IF NOT EXISTS `tbl_slide` (
   `path` varchar(300) NOT NULL,
   `url` varchar(200) NOT NULL,
   `position` tinyint(4) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `tbl_slide`
+--
+
+INSERT INTO `tbl_slide` (`id`, `title`, `img`, `path`, `url`, `position`) VALUES
+(1, 'fdsf', '14665122142245.png', 'uploads/img/slide/14665122142245.png', 'http://www.baidu.com', 3);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_social_account`
+--
+
+DROP TABLE IF EXISTS `tbl_social_account`;
+CREATE TABLE IF NOT EXISTS `tbl_social_account` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `provider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `client_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci,
+  `code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tbl_token`
+--
+
+DROP TABLE IF EXISTS `tbl_token`;
+CREATE TABLE IF NOT EXISTS `tbl_token` (
+  `user_id` int(11) NOT NULL,
+  `code` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `type` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -86,21 +168,24 @@ DROP TABLE IF EXISTS `tbl_user`;
 CREATE TABLE IF NOT EXISTS `tbl_user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` smallint(6) NOT NULL DEFAULT '10',
+  `password_hash` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `confirmed_at` int(11) DEFAULT NULL,
+  `unconfirmed_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `blocked_at` int(11) DEFAULT NULL,
+  `registration_ip` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` int(11) NOT NULL,
-  `updated_at` int(11) NOT NULL
+  `updated_at` int(11) NOT NULL,
+  `flags` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- 转存表中的数据 `tbl_user`
 --
 
-INSERT INTO `tbl_user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'ruige', 'Uw9Cr7vyNRisydmJOjOdo4z7puxQArqW', '$2y$13$6haMj0gb1.jHFcy5Ko0GFOXWWGD2wQNBqkHZHIEiNpBN1Vk.WR/pa', NULL, 'ruige@qq.com', 10, 1466324469, 1466324469);
+INSERT INTO `tbl_user` (`id`, `username`, `email`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`) VALUES
+(1, 'admin', 'ruige@qq.com', '$2y$10$urylcOt9ua6Ev/YKvizoPeB/TxVbGu.jv/KptPjpAVy4H6/r/.qRK', 'MqVZeDU7IBxc-u32SPof1fZkZ5uwcieX', 1466515771, NULL, NULL, '::1', 1466515771, 1466515771, 0);
 
 --
 -- Indexes for dumped tables
@@ -119,19 +204,39 @@ ALTER TABLE `tbl_migration`
   ADD PRIMARY KEY (`version`);
 
 --
+-- Indexes for table `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Indexes for table `tbl_slide`
 --
 ALTER TABLE `tbl_slide`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tbl_social_account`
+--
+ALTER TABLE `tbl_social_account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `account_unique` (`provider`,`client_id`),
+  ADD UNIQUE KEY `account_unique_code` (`code`),
+  ADD KEY `fk_user_account` (`user_id`);
+
+--
+-- Indexes for table `tbl_token`
+--
+ALTER TABLE `tbl_token`
+  ADD UNIQUE KEY `token_unique` (`user_id`,`code`,`type`);
+
+--
 -- Indexes for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
+  ADD UNIQUE KEY `user_unique_email` (`email`),
+  ADD UNIQUE KEY `user_unique_username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -141,17 +246,44 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `tbl_article`
 --
 ALTER TABLE `tbl_article`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `tbl_slide`
 --
 ALTER TABLE `tbl_slide`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tbl_social_account`
+--
+ALTER TABLE `tbl_social_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `tbl_profile`
+--
+ALTER TABLE `tbl_profile`
+  ADD CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE;
+
+--
+-- 限制表 `tbl_social_account`
+--
+ALTER TABLE `tbl_social_account`
+  ADD CONSTRAINT `fk_user_account` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE;
+
+--
+-- 限制表 `tbl_token`
+--
+ALTER TABLE `tbl_token`
+  ADD CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `tbl_user` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
